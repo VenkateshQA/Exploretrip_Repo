cca310153542084eca12b4a659a9c5cd6ef8f20b
muktiyarahmeds <muktiyarahmeds@B2B10.METAMINDS.NET>
2018-11-16 19:02:29 +0530

    login code updatd

--- Exploretrip.com/src/test/java/Login.java
+++ Exploretrip.com/src/test/java/Login.java
@@ -1,9 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class Login {
 
 	public static void main(String[] args) {
 		System.out.print("tesing git");
 
+		
+		System.setProperty("webdriver.chrome.driver","D:\\abdulkalam\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("http://www.tripproagency.com/");
+		driver.findElement(By.id("tripproLoginId")).click();
+		driver.findElement(By.id("signUpAccount-Forget")).click();
+		driver.findElement(By.id("regFirstName")).sendKeys("testagency999@yaho.com");
 	}
 
 }
